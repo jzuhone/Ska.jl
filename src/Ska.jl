9583429dae8cce6e284d5ede0418b2e2ff0fb68c
John ZuHone <jzuhone@gmail.com>
2020-06-15 14:17:19 -0400

    Adding more kadi stuff

--- src/Ska.jl
+++ src/Ska.jl
@@ -4,7 +4,7 @@ module Ska
 export MSID, MSIDset, interpolate
 export CxoTime
 #export XijaModel, make, calc
-export get_states
+export CmdStates, Commands, export_to_dataframe
 
 include("cheta.jl")
 include("time.jl")
@@ -14,7 +14,7 @@ include("kadi.jl")
 
 import .cheta: MSID, MSIDset, interpolate
 import .time: CxoTime
-import .kadi: get_states
+import .kadi: CmdStates, Commands, export_to_dataframe
 
 #import .xija: XijaModel, make, calc
 
--- src/kadi.jl
+++ src/kadi.jl
@@ -3,19 +3,48 @@ module kadi
 import PyCall: PyObject, pyimport, PyNULL, pycall
 import DataFrames: DataFrame
 
-cmd_states = PyNULL()
+kadi_states = PyNULL()
+kadi_commands = PyNULL()
 
 function __init__()
-    copy!(cmd_states, pyimport("kadi.commands.states"))
+    copy!(kadi_states, pyimport("kadi.commands.states"))
+    copy!(kadi_commands, pyimport("kadi.commands"))
 end
 
-function get_states(start, stop; state_keys=nothing, cmds=nothing, 
-                    continuity=nothing, reduce=true, merge_identical=false)
-    states = cmd_states.get_states(start=start, stop=stop, 
-                                   state_keys=state_keys)
-    colnames = states.colnames
+struct Commands
+    cmds_table::PyObject
+    function Commands(cmds_table:PyObject)
+        new(cmds_table)
+    end
+end
+
+struct CmdStates
+    states_table::PyObject
+    function CmdStates(states_table::PyObject)
+        new(states_table)
+    end
+end
+
+function CmdStates(start, stop; state_keys=nothing, continuity=nothing,
+                   reduce=true, merge_identical=false)
+    cmd_states = kadi_states.get_states(start=start, stop=stop, 
+                                        state_keys=state_keys, reduce=reduce,
+                                        merge_identical=merge_identical)
+    CmdStates(cmd_states)
+end
+
+function CmdStates(cmds; state_keys=nothing, continuity=nothing,
+                   reduce=true, merge_identical=false)
+    cmd_states = kadi_states.get_states(cmds=cmds, state_keys=state_keys, 
+                                        reduce=reduce,
+                                        merge_identical=merge_identical)
+    CmdStates(cmd_states)
+end
+
+function export_to_dataframe(cmd_states::CmdStates)
+    colnames = cmd_states.states_table.colnames
     df = DataFrame()
-    getitem = states."__getitem__"
+    getitem = cmd_states.states_table."__getitem__"
     for colname in colnames
         if startswith(colname, "date")
             df[!, colname] = pycall(getitem, Array, colname)
@@ -29,4 +58,17 @@ function get_states(start, stop; state_keys=nothing, cmds=nothing,
     df
 end
 
+function export_to_dataframe(commands::Commands)
+    colnames = commands.cmds_table.colnames
+    df = DataFrame()
+    getitem = commands.cmds_table."__getitem__"
+    for colname in colnames
+        if startswith(colname, "date")
+            df[!, colname] = pycall(getitem, Array, colname)
+        else
+            df[!, colname] = getitem(colname)
+        end    
+        
+    end
+    df
 end
